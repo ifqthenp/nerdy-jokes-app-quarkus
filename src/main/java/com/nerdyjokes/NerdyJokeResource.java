c92061e9baef086b421704cf31d96029a018384b
Andrei Bogomja <bogomja@gmail.com>
2020-09-04 12:36:45 +0100

    Adds logging

--- src/main/java/com/nerdyjokes/NerdyJokeResource.java
+++ src/main/java/com/nerdyjokes/NerdyJokeResource.java
@@ -1,6 +1,8 @@
 package com.nerdyjokes;
 
+import io.vertx.core.http.HttpServerRequest;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.DefaultValue;
@@ -8,11 +10,17 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/api/v1")
 public class NerdyJokeResource {
+
+    @Context
+    HttpServerRequest request;
+
+    private static final Logger LOG = Logger.getLogger(NerdyJokeResource.class);
 
     @Inject
     @RestClient
@@ -23,6 +31,8 @@ public class NerdyJokeResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getNerdyJoke(@DefaultValue("Johnny") @QueryParam("firstName") String firstName,
                                  @DefaultValue("Foobar") @QueryParam("lastName") String lastName) {
+
+        LOG.infof("%s %s %s %s", request.method(), request.path(), request.params().entries(), request.remoteAddress().toString());
 
         return Response.ok(client.getRandomJoke("nerdy", firstName, lastName)).build();
     }
